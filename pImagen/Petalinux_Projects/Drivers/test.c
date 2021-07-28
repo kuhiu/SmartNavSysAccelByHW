430be184d9e63ec22e9e7cda25d5a1f3255a1123
Martin <marfus@hotmail.es>
2021-07-28 00:38:34 -0300

    Primer commit

---
+++ pImagen/Petalinux_Projects/Drivers/test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+
+    printf("Hola mund\n");
+    // int i;
+    // char value;
+    // int total=0;
+    // printf("Ingreso : %s\n", argv[1] );
+    // printf("Ingreso : %d\n", argc );
+    // printf("Ingreso : %d\n", (*argv[1])-48 );
+
+    // total = atoi(argv[1]);
+    // printf("Total es= %d\n", total);
+
+    // if(argc!=2 || ((*argv[1])-48)>7 ) {
+    //     printf("Ha olvidado el valor de los leds o el numero es muy grande\n");
+    //     exit(1);
+    // }
+
+    //printf("Ingreso : %d\n", ((int)argv[1]-48) );
+
+    return 0;
+}
+  
