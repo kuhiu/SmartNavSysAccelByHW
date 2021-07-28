5c8485caf7d965e155c52998805734640e7a17ec
Martin <marfus@hotmail.es>
2021-07-28 00:46:52 -0300

    Primer commit

---
+++ pImagen/Petalinux_Projects/Drivers/pngserver.py
@@ -0,0 +1,26 @@
+import os, png, mmap, BaseHTTPServer
+
+FRAMEBUFFER_OFFSET  = 0x01000000
+WIDTH = 640
+HEIGHT = 480
+PIXEL_SIZE = 3
+
+fh = os.open("/dev/mem", os.O_SYNC | os.O_RDONLY ) # Disable cache, read-only
+mm = mmap.mmap(fh, WIDTH*HEIGHT*PIXEL_SIZE, mmap.MAP_SHARED, mmap.PROT_READ, offset=FRAMEBUFFER_OFFSET)
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(s):
+        writer = png.Writer(WIDTH, HEIGHT, alpha=False, greyscale=False, bitdepth=8)
+        s.send_response(200)
+        s.send_header("Content-type", "image/png")
+        s.end_headers()
+        writer.write_array(s.wfile,[ord(j) for j in mm[0:WIDTH*HEIGHT*PIXEL_SIZE]] )
+
+httpd = BaseHTTPServer.HTTPServer(("0.0.0.0", 80), MyHandler)
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
+httpd.server_close()
+
+mm.close()
+fh.close()
